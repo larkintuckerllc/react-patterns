e4c583d052462d1cfd4cc4186806f2bc07dda734
John Tucker <john@larkintuckerllc.com>
2017-05-25 15:31:16 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Patterns
+
+This respository provides the sample code for a series of articles
+starting with [React By Example: Part 1]
+().
---
+++ reference/index.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+  var counterEl = document.getElementById('counter');
+  var incrementEl = document.getElementById('increment');
+  incrementEl.addEventListener('click', function() {
+    counterEl.innerHTML = (parseInt(counterEl.innerHTML) + 1).toString();
+  });
+})();
